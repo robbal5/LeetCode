e766e20c3456ba33c4e37b97b36fb4eb1772016b
Robby Balistreri <rbalistreri5@gmail.com>
2021-06-16 21:49:58 -0500

    Problem 14

---
+++ leet_code_problems.js
@@ -0,0 +1,20 @@
+// 14 Longest Common Prefix
+var longestCommonPrefix = function (strs) {
+    var testValue;
+    if (strs.any(word => word == '')) return '';
+    if (strs.every(word => word[0] === strs[0][0])) {
+        testValue = strs[0][0]
+    } else {
+        return ''
+    }
+
+    for (let i = 1; i < strs[0].length; i++) {
+        let newVal = testValue + strs[0][i]
+        if (strs.every(word => word.startsWith(newVal))) {
+            testValue = newVal
+        } else {
+            return testValue
+        }
+    }
+    return testValue
+};
