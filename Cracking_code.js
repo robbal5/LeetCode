e6d55c57ee9ae798e6f5328d77f5f5493ebe41b8
Robby Balistreri <rbalistreri5@gmail.com>
2021-06-21 17:08:39 -0500

    URLify

--- Cracking_code.js
+++ Cracking_code.js
@@ -42,3 +42,23 @@ const checkPermutation = function(stringA, stringB) {
 
     
 }
+
+// Time Complexity: O(n) where n is the length of stringA (stringB has same length, if not it returns false immediately) 
+// SpaceComplexity: O(1), as there is a constant max of potential keys in the characters hash
+
+//URLify
+
+const URLify = function(string, length) {
+    let newString = string.slice().split('');
+    for (i = 0; i<length; i++) {
+        console.log(newString)
+        if (newString[i] == ' ') {
+            newString[i] = '%20';
+
+        }
+    }
+    return newString.join('').trim();
+}
+
+//Time Complexity: O(n) where n is the length of the new string
+//Space Complexity: O(n) with the new string to return 
