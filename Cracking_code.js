96a8d1622a8f6de0f86eb25d39cba73851196b5e
Robby Balistreri <rbalistreri5@gmail.com>
2021-06-21 16:50:21 -0500

    Check permutation

--- Cracking_code.js
+++ Cracking_code.js
@@ -2,7 +2,7 @@
 
 // Is Unique: string has all unique characters
 
-const inUnique = function(string) {
+const isUnique = function(string) {
     let characters = {};
     for (i = 0; i< string.length; i++) {
         if (characters[string[i]]) {
@@ -17,3 +17,28 @@ const inUnique = function(string) {
 //Time Complexity: O(n) where n is the length of the string
 // Space complexity: O(1), as constant number of available characters means constant max # of keys in the hash
 
+// Check Permutation
+
+const checkPermutation = function(stringA, stringB) {
+    if (stringA.length != stringB.length) return false;
+    let characters = {};
+
+    for (i = 0; i<stringA.length; i++) {
+        if (characters[stringA[i]]) {
+            characters[stringA[i]] += 1;
+        } else {
+            characters[stringA[i]] = 1
+        }
+    }
+
+    for (j = 0; j<stringB.length; j++) {
+        if (characters[stringB[j]]) {
+            characters[stringB[j]] -= 1
+        } else{ 
+            return false;
+        }
+    }
+    return Object.values(characters).every( count => count === 0)
+
+    
+}
