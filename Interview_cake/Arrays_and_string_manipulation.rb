1373227ffbdff6f8e0a6925ea5d05dec151e8b12
Robby Balistreri <rbalistreri5@gmail.com>
2021-06-30 08:04:16 -0500

    Further testing work

--- Interview_cake/Arrays_and_string_manipulation.rb
+++ Interview_cake/Arrays_and_string_manipulation.rb
@@ -67,4 +67,40 @@ def reverse_in_place(str)
     return str
 end
 
-print reverse_in_place('racecar')
+# print reverse_in_place('racecar')
+
+def reverse_words(words)
+    start_idx = nil
+    last_idx = nil
+    words.each_char.with_index do |char, idx|
+        if (char === ' ' || idx === words.length - 1)
+            while (start_idx < last_idx)
+                words[start_idx], words[last_idx] = words[last_idx], words[start_idx]
+                start_idx += 1
+                last_idx -= 1
+            end
+            start_idx = nil
+            last_idx = nil
+        else
+            start_idx = start_idx || idx
+            last_idx = idx
+        end
+    end
+    return words
+end
+
+print reverse_words('hey there banana man')
+
+def correct_reverse_words(words)
+    let begin_idx = 0;
+    let end_idx = words.length - 1;
+    let start_idx = 0;
+    let last_idx = words.length - 2;
+    while (start_idx < last_idx) 
+        let start_char = words[start_idx];
+        let last_char = words[last_idx];
+        if (start_char == ' ')
+
+            
+    end
+end
