569e82d8db8bc502c13d595c2f0e741cbd3a3355
Robby Balistreri <rbalistreri5@gmail.com>
2021-07-01 13:24:40 -0500

    Find rotating point

---
+++ Interview_cake/sorting_searching.rb
@@ -0,0 +1,33 @@
+def find_rotation_point(array)
+    floor = -1
+    ceiling = array.length
+    base = array[0]
+    while floor + 1 < ceiling
+        distance = ceiling - floor 
+        half_distance = distance/2
+        guess = floor + half_distance
+        if (array[guess - 1] < base)
+            ceiling = guess
+        elsif (array[guess + 1] > base)
+            floor = guess
+        else
+            return array[guess]
+        end
+    end
+    false
+end
+  words = [
+    'ptolemaic',
+    'retrograde',
+    'supplant',
+    'undulate',
+    'xenoepist',
+    'aardvardk',
+    'asymptote',  # <-- rotates here!
+    'babka',
+    'banoffee',
+    'engender',
+    'karpatka',
+    'othellolagkage',
+]
+puts find_rotation_point(words)
