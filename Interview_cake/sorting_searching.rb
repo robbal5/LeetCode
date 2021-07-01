be84e98a4df7023ec8247c791c131a08bc683c67
Robby Balistreri <rbalistreri5@gmail.com>
2021-07-01 15:48:42 -0500

    find duplicates

--- Interview_cake/sorting_searching.rb
+++ Interview_cake/sorting_searching.rb
@@ -30,4 +30,39 @@ end
     'karpatka',
     'othellolagkage',
 ]
-puts find_rotation_point(words)
+# puts find_rotation_point(words)
+
+def find_duplicate(array)
+    floor = 1
+    ceiling = array.length - 1
+    while floor < ceiling
+        midpoint = floor + (ceiling - floor) / 2
+        # puts midpoint
+        lower_range_floor = floor
+        lower_range_ceiling = midpoint
+        upper_range_floor = midpoint + 1
+        upper_range_ceiling = ceiling
+
+        total_lower_range = array.count do |num|
+            num >= lower_range_floor && num <= lower_range_ceiling
+        end
+        print 'midpoint:'
+        print midpoint
+        puts 
+        print 'total_lower_range:'
+        print total_lower_range
+        puts
+
+        total_lower_possible = lower_range_ceiling - lower_range_floor + 1
+        # puts lower_range_ceiling
+        if total_lower_range > total_lower_possible
+            floor, ceiling = lower_range_floor, lower_range_ceiling
+        else
+            floor, ceiling = upper_range_floor, upper_range_ceiling
+        end
+    end
+    floor
+end
+
+dup_arr = [1,2,3,3,4,5,6,7,8,9]
+puts find_duplicate(dup_arr)
