5de57a1379f4b085a61eabe4d8d08c8a17c6d245
Robby Balistreri <rbalistreri5@gmail.com>
2021-07-01 10:04:38 -0500

    Finalize product of 3

---
+++ Interview_cake/Greedy_algorithms.rb
@@ -0,0 +1,101 @@
+def apple_stocks(stocks)
+    buy_index = 0;
+    sell_index = 1;
+    max_profit = stocks[sell_index] - stocks[buy_index]
+    while(sell_index < stocks.length)
+            if (stocks[sell_index] - stocks[buy_index] > max_profit)
+                max_profit = stocks[sell_index] - stocks[buy_index]
+            end
+        if (stocks[sell_index] < stocks[buy_index])
+            buy_index = sell_index
+            sell_index += 1
+        else
+            sell_index += 1
+        end
+    end
+    max_profit
+end
+
+# puts apple_stocks([19, 17, 14, 12, 5, 0])
+# DOESN't WORK W Negatives
+def product_of_three(arr)
+    third, second, first = [arr[0], arr[1], arr[2]].sort
+    (arr.length - 3).times do |n|
+        n += 3
+        if (arr[n] > third)
+            if (arr[n] > second)
+                if (arr[n] > first)
+                    third, second, first = second, first, arr[n]
+                    next
+                end
+                third, second = second, arr[n]
+                next
+            end
+            third = arr[n]
+        end
+    end
+    puts third
+    puts second
+    puts first
+    third * second * first
+end
+
+# puts product_of_three([1,4,12,2,15,4,100])
+
+# Still doesn't work with negatives
+def product_of_three2(arr)
+    third, second, first = [arr[0], arr[1], arr[2]].sort
+    max_product = first * second * third
+    arr.each_with_index do |num, idx|
+        next if [0,1,2].include?(idx)
+        let sub_products = [[third, second], [second, first], [first, third]]
+        sub_products.each do |product|
+            if (product[0] * product[1] * num > max_product)
+                max_product = product[0] * product[1] * num
+                first, second, third =[product[0], product[1], num]
+            end
+        end
+    end
+    return max_product
+end
+# puts product_of_three([1,-4,12,2,-50,4,100])
+
+def product_of_three3(arr)
+    highest_product_of_3 = arr[0] * arr[1] * arr[2]
+    highest_product_of_2 = arr[0] * arr[1]
+    highest = arr[0]
+    lowest_product_of_2 = arr[0] * arr[1]
+    lowest = arr[0]
+
+    arr.each_with_index do |num, idx|
+        if (idx == 0)
+            next
+        end
+        if (idx == 1)
+            highest = arr[1] if arr[1] > highest
+            lowest = arr[1] if arr[1] < lowest
+            next
+        end
+        if (idx == 2)
+            lowest_product_of_2 = arr[2] * lowest if (arr[2] * lowest < lowest_product_of_2)
+            highest_product_of_2 = arr[2] * lowest if (arr[2] * highest > highest_product_of_2)
+            highest = arr[2] if arr[2] > highest
+            lowest = arr[2] if arr[2] < lowest
+            next 
+        end
+        if (highest_product_of_3 < highest_product_of_2 * num)
+            highest_product_of_3 = highest_product_of_2 * num
+        elsif (highest_product_of_3 < lowest_product_of_2 * num)
+            highest_product_of_3 = lowest_product_of_2 * num
+        end
+            lowest_product_of_2 = num * lowest if (num * lowest < lowest_product_of_2)
+            highest_product_of_2 = num * lowest if (num * highest > highest_product_of_2)
+            highest = num if num > highest
+            lowest = num if num < lowest
+    end
+    highest_product_of_3
+
+    
+end
+array_of_ints = [-1000, -10, -5, 1, -100, 12, -8]
+puts product_of_three3(array_of_ints)
