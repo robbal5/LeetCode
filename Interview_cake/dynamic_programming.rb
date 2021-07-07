732fe9ad0758f565568d1b7ab3c90e87c135152d
Robby Balistreri <rbalistreri5@gmail.com>
2021-07-07 15:14:42 -0500

    Make change

--- Interview_cake/dynamic_programming.rb
+++ Interview_cake/dynamic_programming.rb
@@ -13,4 +13,57 @@ def permutations(string)
     permutations.uniq
 end
 
-puts permutations('hello').length
+# puts permutations('hello').length
+
+def nth_fibonacci(num)
+    second_to_last = 0
+    last = 1
+    number = 2
+    while number <= num
+        second_to_last, last = last, second_to_last + last
+        number += 1
+    end
+   last
+end
+
+# puts nth_fibonacci(5)
+# puts nth_fibonacci(7)
+
+#Double counted, not correct
+def make_change(total, denominations)
+    ways = {}
+    start_point = 1
+    while start_point <= total
+        sub_answer = 0
+        denominations.each do |coin|
+            if coin == start_point
+                sub_answer += 1
+            elsif coin < start_point
+                next if (denominations.include?(start_point - coin) && coin < (start_point - coin))
+                sub_answer += ways[start_point - coin]
+            end
+        end
+        ways[start_point] = sub_answer
+        start_point += 1
+    end
+    puts ways
+end
+
+
+
+def make_change2(total, denominations)
+    ways = [1] + Array.new(total, 0)
+
+    denominations.each do |coin|
+
+        (coin..total).each do |amount|
+            remainder = amount - coin
+            ways[amount] += ways[remainder]
+        end
+        print ways
+        puts
+    end
+    ways[total]
+end
+
+puts make_change2(4,[1,2,3])
