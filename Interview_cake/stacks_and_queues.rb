bd80159a0611d05345d6edb98f592b90f651edde
Robby Balistreri <rbalistreri5@gmail.com>
2021-07-11 10:53:07 -0500

    parentheses checker

--- Interview_cake/stacks_and_queues.rb
+++ Interview_cake/stacks_and_queues.rb
@@ -95,3 +95,52 @@ end
 # tester.enqueue('mister')
 # puts tester.dequeue
 
+def parenthetical_matcher(sentence, pos)
+
+  par_number = 0
+  test_number = nil
+  sentence.each_char.with_index do |char, idx|
+    if char == '('
+      par_number += 1
+    elsif char == ')'
+      par_number -=1
+      return idx if test_number && test_number > par_number
+    end
+    if idx == pos
+      test_number = par_number
+    end
+  end
+  nil
+end
+
+# puts parenthetical_matcher("Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing.", 10)
+
+def parentheses_checker(string)
+  opening_par = {
+    '{' => 1,
+    '(' => 2,
+    '[' => 3
+  }
+  closing_par = {
+    '}' => 1,
+    ')' => 2,
+    ']' => 3
+  }
+
+  stack = []
+
+  string.each_char do |char|
+    if opening_par.has_key?(char)
+      stack << char
+    elsif closing_par.has_key?(char)
+      if (closing_par[char] == opening_par[stack.last])
+        stack.pop
+      else
+        return false
+      end
+    end
+  end
+  stack.length == 0
+
+
+end
