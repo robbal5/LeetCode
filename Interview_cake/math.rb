2ffb78e449b61f1dc9eec901407ab65bef1134a6
Robby Balistreri <rbalistreri5@gmail.com>
2021-07-14 11:15:29 -0500

    Check integer in sorted

--- Interview_cake/math.rb
+++ Interview_cake/math.rb
@@ -6,3 +6,22 @@ def appears_twice(arr)
     end
     triangular_sum
 end
+
+def check_integer_in_sorted(arr, target)
+    return nil if arr.length == 0
+    middle = arr.length / 2
+    check_value = arr[middle]
+    case check_value <=> target
+    when -1
+        return 1 + middle + check_integer_in_sorted(arr[middle+1.. -1], target)
+    when 0
+        return middle
+    when 1
+        return check_integer_in_sorted(arr[0...middle], target)
+    end
+end
+
+sorted = [1,2,3,4,5,6,7,8,9,10]
+
+puts check_integer_in_sorted(sorted, 8)
+# puts check_integer_in_sorted(sorted, 3)
