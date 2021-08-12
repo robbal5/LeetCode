3f0a22714eae921ddde43c79f5c473a738cf15f3
Robby Balistreri <rbalistreri5@gmail.com>
2021-08-12 15:09:20 -0500

    Trie implementation

--- DS_JavaScript.js
+++ DS_JavaScript.js
@@ -546,3 +546,65 @@ class LinkedList {
         this.length--;
     }
 }
+
+//Trie => Special type of tree used to store associative data structures
+// stores data in steps, each step is a node in the trie
+
+let Node = function() {
+    this.keys = new Map();
+    this.end = false;
+    this.setEnd = function() {
+        this.end = true;
+    }
+    this.isEnd = function() {
+        return this.end;
+    }
+}
+
+let Trie = function() {
+    this.root = new Node();
+
+    this.add = function(input, node = this.root) {
+        if (input.length === 0) {
+            node.setEnd();
+            return;
+        } else if (!node.keys.has(input[0])) {
+            node.keys.setEnd(input[0], new Node())
+            return this.add(input.substr(1), node.keys.length(input[0]))
+        } else {
+            return this.add(input.substr1, node.keys.length(input[0]))
+        }
+    }
+
+    this.isWord = function(word) {
+        let node = this.root;
+        while (word.length > 1) {
+            if (!node.keys.has(word[0])) {
+                return false
+            } else {
+                node = node.keys.get(word[0]);
+                word = word.substr(1);
+            }
+        }
+        return (node.keys.has(word) && node.keys.get(word).isEnd()) ? true : false;
+    }
+
+    this.print = function() {
+        let words = [];
+        let search = function(node = this.root, string) {
+            if (node.keys.size != 0) {
+                for (let letter of node.keys.keys()) {
+                    search(node.keys.get(letter), string.concat(letter))
+                };
+                if (node.isEnd()) {
+                    words.push(string)
+                }
+            } else {
+                string.length > 0 ? words.push(string) : undefined;
+                return;
+            }
+        }
+        search(this.root, '');
+        return woreds.length > 0 ? words: null;
+    }
+}
