3c1ca734993ccd581f840e9e7b064c43af613df1
Robby Balistreri <rbalistreri5@gmail.com>
2021-09-06 14:03:55 -0500

    Ruby problems

--- 75_Problems.rb
+++ 75_Problems.rb
@@ -18,3 +18,38 @@ def max_profit(prices)
     end
     curr_max_profit
 end
+
+require 'set'
+def contains_duplicate(nums)
+    numsSet = Set.new()
+    nums.each do |num|
+        return true if numsSet.include?(num) 
+        numsSet.add(num)
+    end
+    false
+end
+
+def product_except_self(nums)
+    left_result = [1]
+    for i in (1...nums.length)
+        left_result << left_result[i - 1] * nums[i - 1] 
+    end
+    right_result = Array.new(nums.length)
+    j = nums.length - 1
+    right_result[j] = 1
+    j-=1
+    while (j >= 0)
+        right_result[j] = nums[j + 1] * right_result[j + 1]
+        j-=1
+    end
+    right_result.zip(left_result).map { |a,b| a*b}
+end
+def max_sub_array(nums)
+    curr_max_sum = nums[0]
+    curr_sum = nums[0]
+    for i in (1...nums.length)
+        curr_sum = curr_sum + nums[i] >= nums[i] ? curr_sum + nums[i] : nums[i]
+        curr_max_sum = [curr_sum, curr_max_sum].max
+    end
+    curr_max_sum
+end
